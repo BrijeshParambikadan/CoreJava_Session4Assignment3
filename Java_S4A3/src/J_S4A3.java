3c2177aabdd961606254eb6d0541dd7d9d484d62
BrijeshParambikadan <bbavittom@gmail.com>
2016-10-17 22:30:37 +0530

    submitted JavaS4A3 assignment

---
+++ Java_S4A3/src/J_S4A3.java
@@ -0,0 +1,32 @@
+
+public class J_S4A3 {
+
+	public static void main(String[] args) {
+		/*
+		 * Assume that class A extends class B, 
+		 * which extends class C. Also all the 
+		 * three classes implement the method test(). 
+		 * How can a method in a class A invoke the 
+		 * test() method defined in class C 
+		 * (without creating a new instance of class C). 
+		 * Select the one correct answer.
+		 * 
+		 * Select the one correct answer.
+a. test();
+b. super.test();
+c. super.super.test();
+d. ::test();
+e. C.test();
+f. It is not possible to invoke test() method defined in C from a method in A
+		 * */
+	}
+
+}
+
+// Answer is as follows:
+
+/*
+ * f. It is not possible to invoke test() method defined in C from a method in A.
+ */
+
+// Thank You
